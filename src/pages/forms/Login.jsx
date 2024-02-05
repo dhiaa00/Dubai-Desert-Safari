8e2fd084ad8a1cbca3ccd15e02e81989e63bd93e
dhiaa00 <dhiaatipaza42@gmail.com>
2024-02-05 16:31:05 +0100

    login page added

--- src/pages/forms/Login.jsx
+++ src/pages/forms/Login.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import "./login.css";
 
-const s = () => {
-  return <div>s</div>;
+const Login = () => {
+  return (
+    <form className="login-form" action="">
+      <div className="form-wrapper">
+        <div className="email-wrapper">
+          <label htmlFor="email">Email</label>
+          <div className="info-input">
+            <i class="bi bi-envelope-at"></i>
+            <input id="email" type="email" placeholder="example@gmail.com" />
+          </div>
+        </div>
+        <div className="password-wrapper">
+          <label htmlFor="password">Password</label>
+          <div className="info-input">
+            <i class="bi bi-shield-lock"></i>
+            <input
+              id="password"
+              type="password"
+              placeholder="Enter your password"
+            />
+          </div>
+        </div>
+      </div>
+    </form>
+  );
 };
 
-export default s;
+export default Login;
