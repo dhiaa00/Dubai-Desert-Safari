9256953c65da8fbf86b7fb51ec35afb902d8d0f1
dhiaa00 <dhiaatipaza42@gmail.com>
2024-02-09 11:28:40 +0100

    login in page completed

--- src/pages/forms/Login.jsx
+++ src/pages/forms/Login.jsx
@@ -1,9 +1,14 @@
-import { Slide, toast } from "react-toastify";
+import { useState } from "react";
+import { Slide, ToastContainer, toast } from "react-toastify";
 import "./login.css";
+import "react-toastify/dist/ReactToastify.css";
 
 const Login = () => {
-  const notify = (msg) => {
-    toast.warn(msg, {
+  const [emailvalue, setEmailvalue] = useState("");
+  const [passwordvalue, setPasswordvalue] = useState("");
+
+  const notifyerror = (msg) => {
+    toast.error(msg, {
       position: "top-right",
       autoClose: 5000,
       hideProgressBar: false,
@@ -11,50 +16,84 @@ const Login = () => {
       pauseOnHover: true,
       draggable: true,
       progress: undefined,
-      theme: "light",
+      theme: "colored",
+      transition: Slide,
+    });
+  };
+  const notifyvalidation = (msg) => {
+    toast.success(msg, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
       transition: Slide,
     });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (e.type === "email") {
-      const emailReg = new RegExp("[^A-Za-z]+\\d*@[A-Za-z]+\\.[A-Za-z]+");
-      console.log(emailReg.test(e.value));
+    const emailRegex = new RegExp("^[A-Za-z]+\\d*@[A-Za-z]+\\.[A-Za-z]{2,}$");
+    if (!emailRegex.test(emailvalue)) {
+      notifyerror("Email adress non valide");
+    } else if (passwordvalue.length < 8) {
+      notifyerror("Password should contain at least 8 characters");
+    } else {
+      setEmailvalue("");
+      setPasswordvalue("");
+      notifyvalidation("Logged in");
     }
   };
 
   return (
-    <form className="login-form" action="" method="POST">
-      <div className="form-wrapper">
-        <div className="email-wrapper">
-          <label htmlFor="email">Email</label>
-          <div className="info-input">
-            <i className="bi bi-envelope-at"></i>
-            <input
-              id="email"
-              type="email"
-              placeholder="example@gmail.com"
-              onChange={(e) => console.log(e.target.type)}
-            />
+    <>
+      <ToastContainer />
+      <form
+        className="login-form"
+        action=""
+        method="POST"
+        onSubmit={(e) => handleSubmit(e)}>
+        <div className="form-wrapper">
+          <div className="email-wrapper">
+            <label htmlFor="email">Email</label>
+            <div className="info-input">
+              <i className="bi bi-envelope-at"></i>
+              <input
+                id="email"
+                placeholder="example@gmail.com"
+                value={emailvalue}
+                onChange={(e) => {
+                  setEmailvalue(e.target.value);
+                }}
+                formNoValidate
+              />
+            </div>
           </div>
-        </div>
-        <div className="password-wrapper">
-          <label htmlFor="password">Password</label>
-          <div className="info-input">
-            <i className="bi bi-shield-lock"></i>
-            <input
-              id="password"
-              type="password"
-              placeholder="Enter your password"
-            />
+          <div className="password-wrapper">
+            <label htmlFor="password">Password</label>
+            <div className="info-input">
+              <i className="bi bi-shield-lock"></i>
+              <input
+                id="password"
+                type="password"
+                placeholder="Enter your password"
+                formNoValidate
+                value={passwordvalue}
+                onChange={(e) => {
+                  setPasswordvalue(e.target.value);
+                }}
+              />
+            </div>
           </div>
+          <button type="submit" className="login-button">
+            Login
+          </button>
         </div>
-        <button type="submit" className="login-button">
-          Login
-        </button>
-      </div>
-    </form>
+      </form>
+    </>
   );
 };
 
